f1343420476d87ecca44e51bdd77d7227c159133
pivovar-samara <pivovar63dev@gmail.com>
2025-12-20 17:08:25 +0400

    fix prebuild script with correct paths

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -euo pipefail
 
-cd "${CI_WORKSPACE}"
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+PROJECT_ROOT="$(cd "${SCRIPT_DIR}/.." && pwd)"
+
+cd "${PROJECT_ROOT}"
 
 mkdir -p Configs
 
